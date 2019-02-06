dc9b00c8b8a7152fb6fc25d92f939592b7ebf03b
steve-dlp <steve@doublelinepartners.com>
2019-02-06 16:33:08 -0600

    Updating with identies API setup in program where school year is declared

--- MDE-EdFiClientSDK/EdFi/OdsApi/Program.cs
+++ MDE-EdFiClientSDK/EdFi/OdsApi/Program.cs
@@ -1,59 +1,63 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using EdFi.OdsApi.Api;
+using EdFi.OdsApi.Models;
 using EdFi.OdsApi.Sdk;
-using EdFi.OdsApi.Models.Resources;
-using EdFi.OdsApi.Api.Resources;
 using RestSharp;
 
-namespace MNEdFiClientSDK
+using EdFi.OdsApi.Models.MinnesotaSISVendorProfile;
+using EdFi.OdsApi.Models.Resources;
+using EdFi.OdsApi.Api.Resources;
+
+namespace EdFiClientSDK
 {
     class Program
     {
         static void Main(string[] args)
-        {// Trust all SSL certs -- needed unless signed SSL certificates are configured.
-            System.Net.ServicePointManager.ServerCertificateValidationCallback =
+        {
+            // Trust all SSL certs -- needed unless signed SSL certificates are configured.
+            ServicePointManager.ServerCertificateValidationCallback =
                 ((sender, certificate, chain, sslPolicyErrors) => true);
-
-            //Explicitly configures outgoing network calls to use the latest version of TLS where possible.
-            //Due to our reliance on some older libraries, the.NET framework won't necessarily default
-            //to the latest unless we explicitly request it. Some hosting environments will not allow older versions
-            //of TLS, and thus calls can fail without this extra configuration.
-            System.Net.ServicePointManager.SecurityProtocol |= System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12;
 
             // OAuth configuration
             var oauthUrl = "https://test.edfi.education.mn.gov/EdFi.Ods.WebApi/";
-            var clientKey = "zAE28pM3Tzzz";
-            var clientSecret = "eqqTCngL3s1FyJ7rq6m3czzz";
+            var clientKey = "j57lM8oL5S7O";
+            var clientSecret = "h9kgUASBEQhojU8HiSCsNPjz";
 
             // RestSharp dependency, install via NuGet
             var client = new RestClient("https://test.edfi.education.mn.gov/EdFi.Ods.WebApi/api/v2/2018/");
 
+         
             // TokenRetriever makes the OAuth calls
             var tokenRetriever = new TokenRetriever(oauthUrl, clientKey, clientSecret);
 
-            // Plug Oauth into RestSharp's authentication scheme
+            // Plug OAuth into RestSharp's authentication scheme
             client.Authenticator = new BearerTokenAuthenticator(tokenRetriever);
 
             // GET schools
             var api = new SchoolsApi(client);
-
             var response = api.GetSchoolsAll(null, null); // offset, limit
 
             var httpReponseCode = response.StatusCode; // returns System.Net.HttpStatusCode.OK
-            var schools = response.Data;
-
             Console.WriteLine("Response code is " + httpReponseCode);
+            Console.WriteLine();
+            
+            // Create identity example setup
+            //var IdentityApi = new identitiesApi(client, 2019);
+            //var Identitiesresponse = IdentityApi.IdentitiesV2_Create(null);
+            
+            
+            // Response data is serialized directly to typed model classes
+            List<School> schools = response.Data;
 
             foreach (var school in schools)
-            {
-                Console.WriteLine(school.nameOfInstitution);
-            }
+                Console.WriteLine(school.schoolId + ": " + school.nameOfInstitution);
+
             Console.WriteLine();
             Console.WriteLine("Hit ENTER key to continue...");
             Console.ReadLine();
         }
     }
 }
+
