2c436e6ededccbd06613ce609d54cc65e81d3b13
jolivas-dl <j.olivas@wearedoubleline.com>
2022-05-02 20:20:58 -0500

    update jar version

--- MDE-EdFiClientSDK/EdFi/generate-sdk.ps1
+++ MDE-EdFiClientSDK/EdFi/generate-sdk.ps1
@@ -1,12 +1,12 @@
 # TechDocs: https://techdocs.ed-fi.org/display/ODSAPI31/Using+Code+Generation+to+Create+an+SDK
 # URL Source: https://test.edfi.education.mn.gov/sb21_/edfi.ods.webapi/metadata
 $javaExecutable = "java.exe"
-$swaggerCodeGenJar = "swagger-codegen-cli-2.4.15.jar"
+$swaggerCodeGenJar = "swagger-codegen-cli-2.4.27.jar"
 $metaDataUrl = "https://test.edfi5.education.mn.gov/api/metadata/"
 $outputDir = "generated-sdk"
 
 if (-not (Test-Path $swaggerCodeGenJar)) {
-    Invoke-WebRequest -OutFile $swaggerCodeGenJar "https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.4.15/$swaggerCodeGenJar"
+    Invoke-WebRequest -OutFile $swaggerCodeGenJar "https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.4.27/$swaggerCodeGenJar"
 }
 
 if (Test-Path $outputDir) { Remove-Item -Force -Recurse $outputDir }
